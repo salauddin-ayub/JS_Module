e46ddc0e24a1cfded2c04a8f8170db8e4b5cbec4
SalauddinAyub <ayub35-265@diu.edu.bd>
2021-10-10 09:49:34 +0600

    factorial, fibonacci & prime number

---
+++ Jhonkar_Javascript/factorial.js
@@ -0,0 +1,10 @@
+function factorial(n){
+    var factorial = 1;
+    for (var i = 1; i<= n; i++){
+        factorial = factorial * i;
+
+    }
+    return factorial;
+}
+result = factorial(5);
+console.log(result);
